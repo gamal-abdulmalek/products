ef29351647a5c932777955ce616eda6650721a4f
gamal-abdulmalek <gemi2015y@gmail.com>
2024-12-08 22:53:54 +0300

    create product routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::apiResource("products",ProductController::class);
