8bdfd87c007e4dd3a0a9561efc4f372ecd87ddcf
gamal-abdulmalek <gemi2015y@gmail.com>
2024-12-08 23:36:00 +0300

    add fields fillable names to product model

--- api/app/Models/product.php
+++ api/app/Models/product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
+    protected $fillable = ['name', 'description', 'photo','price','SKU','minQty','category_id'];
     public function category(){
         return $this->belongsTo(Category::class);
     }
