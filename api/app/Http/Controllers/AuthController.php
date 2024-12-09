dc1f78fb0e6963ff56b3a5483f69cb11851f652f
gamal-abdulmalek <gemi2015y@gmail.com>
2024-12-09 23:56:18 +0300

    create authcontroller

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    
+}
