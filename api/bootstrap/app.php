8492afa6629ba6d0c7cb5d20323daf4c8e61cdd2
gamal-abdulmalek <gemi2015y@gmail.com>
2024-12-14 00:02:57 +0300

    fix bootstrap/app

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -12,7 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            
+        ]);
+        $middleware->validateCsrfTokens(except: [
+            '*/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
